9db9acba3eb9f140d4f0581d4a4ec27397ef5238
Pavithra-Devadiga <pavithradevadiga2006@gmail.com>
2026-03-23 01:18:56 +0530

    fix github pages path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [],
-  base: '/Weather/',
-  build: {
-    outDir: 'docs'
-  }
+  plugins: [react()],
+  base: '/breeze-weather-site/'
 })
